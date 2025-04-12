f0c3226f6f0ca358e888f52fc80e361f9900e51e
Adelodun Sukurat Gbanjubola <131052279+Dottie1234@users.noreply.github.com>
2025-04-12 17:38:08 +0100

    Add files via upload
    Updated data and code

--- frontier_pipeline.py
+++ frontier_pipeline.py
@@ -1,27 +1,20 @@
-# import libraries
 from prefect import flow, task
 import pandas as pd
 from bs4 import BeautifulSoup
 import requests
 import datetime
-from datetime import timedelta
+#from datetime import timedelta
 
-# declare tasks
 @task
 def scrape_frontier_data():
-    # url
     url = 'http://frontierclimate.com/portfolio'
-
-    # get html
     response = requests.get(url)
     soup = BeautifulSoup(response.text, 'html.parser')
     infor = soup.find_all('div', class_='mb-4')
 
-    # data lists
-    company, description, tracks, contracted_tons, locations, pathways = [], [], [], [], [], []
+    company, description, tracks, contracted_tons, locations, pathways, links = [], [], [], [], [], [], []
 
     for info in infor:
-        # get data
         name = info.find('h3', class_='mb-3').get_text(strip=True)
         desc = info.find('p').get_text(" ", strip=True)
         track = info.find('dd').get_text(strip=True)
@@ -30,7 +23,13 @@ def scrape_frontier_data():
         location = ddtext[1].get_text(strip=True)
         pathway = info.find('div', class_='text-black').get_text(strip=True) if info.find('div', class_='text-black') else None
 
-        # append data to list
+        a_tag = info.find('a', class_='rounded')
+        if a_tag:
+            tag = str(a_tag.get('href'))
+            links.append('https://frontierclimate.com' + tag)
+        else:
+            links.append('empty')
+
         company.append(name)
         description.append(desc)
         tracks.append(track)
@@ -38,30 +37,70 @@ def scrape_frontier_data():
         locations.append(location)
         pathways.append(pathway)
 
-    # put data in dataframe
+    soups = []
+
+    for link in links:
+        if link != 'empty':
+            response = requests.get(link)
+            soup = BeautifulSoup(response.text, 'html.parser')
+            soups.append(soup)
+
+        else: 
+            soups.append('empty')
+            
+    tons, contract_value, deliverytimeline = [], [], []
+
+    for soup in soups:
+        if soup != 'empty':
+            infor = soup.find_all('div', class_ = 'grid')
+            for info in infor:
+                contract = info.find_all('dd', class_ = 'text-[14px]')
+                ton = contract[0].get_text(strip = True)
+                value = contract[1].get_text(strip = True)
+                timeline = contract[3].get_text(strip = True)
+
+                
+                tons.append(ton)
+                contract_value.append(value)
+                deliverytimeline.append(timeline)
+        else: 
+            contract_value.append(0)
+            tons.append('empty')
+            deliverytimeline.append(None)
+
+
     df = pd.DataFrame({
         'Company': company,
         'Description': description,
         'Track': tracks,
         'Contracted_tons': contracted_tons,
         'Location': locations,
-        'Pathways': pathways
+        'Pathways': pathways,
+        'Tons': tons,
+        'Contracted value': contract_value,
+        'Delivery timeline': deliverytimeline
     })
 
-    # save to csv
+
     df.to_csv('Frontier_data.csv', index=False)
     return df
 
+#data = pd.read_csv('Frontier_data.csv')
+
 @task
 def clean_data(df):
-    # convert to numeric
+    
     df['Contracted_tons'] = df['Contracted_tons'].str.replace(',', '')
     df['Contracted_tons'] = df['Contracted_tons'].str.replace('-', '0')
+    df['Tons'] = df['Tons'].apply(lambda x: '0' if x == 'empty' else x.replace(',', '')) 
+    df['Tons'] = df['Tons'].apply(lambda x: '0' if x == 'empty' else x.replace('-', '0')) 
+    df['Tons'] = df['Tons'].astype('int32')
     df['Contracted_tons'] = df['Contracted_tons'].astype('int32')
+
+    df.fillna('not specified', axis=1, inplace=True)
     df.to_csv('cleaned_frontier.csv', index=False)
     return df
 
-# declare flow
 @flow(name="frontierpipeline")
 def frontier_pipeline():
     print(f" Flow started at {datetime.datetime.now()}")
@@ -69,14 +108,15 @@ def frontier_pipeline():
     cleaned_df = clean_data(raw_df)
     print(" Flow finished successfully")
 
-#call flow
 if __name__ == "__main__":
-    frontier_pipeline()
+    frontier_pipeline.serve(name = 'frontier_deployment', 
+                            interval  = 3600)
 
 #Check out the dashboard at http://127.0.0.1:4200
 '''
 prefect server start
 
+PREFECT_API_URL=http://127.0.0.1:4200/api prefect work-pool create --type process frontier-pool
 
 prefect work-pool create frontier-pool --type process
 
@@ -88,3 +128,15 @@ prefect deployment run frontierpipeline/daily-frontier
 
 prefect flow-run ls
 '''
+
+'''
+python deployment.py
+prefect worker start -p default
+
+To run on demand
+prefect deployment run frontier_pipeline/frontier-scheduled-deployment
+
+
+
+'''
+
